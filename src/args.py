25387d3df4cba985ad9e5bdd4029787857d54254
e-hossam96 <ehh.hossam@gmail.com>
2024-01-10 22:23:04 +0200

    added access tokens path to preprocessing args

--- src/args.py
+++ src/args.py
@@ -24,5 +24,11 @@ def get_processing_parser():
         default="./data/vector_store/",
         help="The directory to save or load the Chroma db to/from",
     )
+    parser.add_argument(
+        "--access_tokens_path",
+        type=str,
+        default="./data/access_tokens.json",
+        help="The directory to access credentials",
+    )
 
     return parser
