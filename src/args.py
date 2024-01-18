1290795e15062934a801c5a7e463beecd7bbcb58
e-hossam96 <ehh.hossam@gmail.com>
2024-01-18 21:44:35 +0200

    restructured args

--- src/args.py
+++ src/args.py
@@ -3,21 +3,9 @@
 import argparse
 
 
-def get_processing_parser():
-    """Create argparser for data preprocessing."""
+def get_base_parser():
+    """Create base argparser shared by all calls."""
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--docs_dir",
-        type=str,
-        required=True,
-        help="The directory containing the wandb documentation",
-    )
-    parser.add_argument(
-        "--chunk_size",
-        type=int,
-        default=500,
-        help="The number of tokens to include in each document chunk",
-    )
     parser.add_argument(
         "--vector_db",
         type=str,
@@ -30,5 +18,34 @@ def get_processing_parser():
         default="./data/access_tokens.json",
         help="The directory to access credentials",
     )
+    return parser
 
+
+def get_processing_parser():
+    """Create argparser for data preprocessing."""
+    parser = get_base_parser()
+    parser.add_argument(
+        "--docs_dir",
+        type=str,
+        required=True,
+        help="The directory containing the wandb documentation",
+    )
+    parser.add_argument(
+        "--chunk_size",
+        type=int,
+        default=500,
+        help="The number of tokens to include in each document chunk",
+    )
+    return parser
+
+
+def get_chat_parser():
+    """Create argparser for convesation."""
+    parser = get_base_parser()
+    parser.add_argument(
+        "--prompt_temp_path",
+        type=str,
+        default="./data/prompt_template.json",
+        help="The directory to prompt templates.",
+    )
     return parser
